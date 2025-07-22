0eec2f041a40d4b4e9e4f52f4fb2b0846d841828
prismaalamsyah <prismaalamsyah1@gmail.com>
2025-07-22 22:18:47 +0700

    first commit

--- tokenList.ts
+++ tokenList.ts
@@ -1,12 +1,12 @@
 export const TOKEN_LIST = [
   {
     symbol: "TRN",
-    address: "0xTRNTokenAddress",
+    address: "0x1114982539A2Bfb84e8B9e4e320bbC04532a9e44",
     decimals: 18
   },
   {
     symbol: "USDT",
-    address: "0xUSDTTokenAddress",
+    address: "0xfd086bc7cd5c481dcc9c85ebe478a1c0b69fcbb9",
     decimals: 6
   }
 ];
